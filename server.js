9ea0feaf28f04a8e0ba0af5318ebcdb226fd7b84
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-06 16:20:59 +0500

    Server Code:

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+// import authRoutes from './routes/auth.routes.js';
+// import expenseRoutes from './routes/expense.routes.js';
+// import userRoutes from './routes/user.routes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/expenses', expenseRoutes);
+// app.use('/api/users', userRoutes);
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
