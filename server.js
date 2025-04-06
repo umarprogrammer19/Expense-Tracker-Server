9867f7e6ec7fec5515328a09a829cfb42815a79f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-06 16:37:56 +0500

    Mongo DB

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ app.use(express.json());
 // app.use('/api/users', userRoutes);
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: "expense_tracker"
+})
+    .then(() => {
+        console.log('MongoDB connected Successfully');
+        app.listen(process.env.PORT, () =>
+            console.log(`Server running on port ${process.env.PORT}`)
+        );
+    })
+    .catch(err => console.error(err));
